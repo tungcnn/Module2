a24f3582fccfa31c57c95f51dff013af9cd3d192
tungcnn <sontung.cnn@gmail.com>
2021-01-07 16:35:12 +0700

    Add Tinh ngay trong thang

---
+++ IntroToJava/Tinh so ngay trong thang/src/App.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Which month that you want to count days: ");
+        int month = scanner.nextInt();
+        String days;
+        switch(month) {
+            case 2:
+                days = "28 or 29";
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                days = "31";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = "30";
+                break;
+            default:
+                days = "";
+                break;
+        }
+        if (days != "") System.out.printf("The month '%d' has %s days!", month, days);
+        else System.out.print("Ïnvalid input!");
+    }
+}
