38daf2da1a9c757340d631e66198e68dec56f024
tungcnn <sontung.cnn@gmail.com>
2021-01-07 15:30:37 +0700

    Add thuc hanh khai bao

--- IntroToJava/SystemTime/src/SystemTime.java
+++ IntroToJava/SystemTime/src/SystemTime.java
@@ -1,8 +1,10 @@
+import java.net.SocketPermission;
 import java.util.Date;
 
 public class SystemTime {
     public static void main(String[] args) {
         Date now = new Date();
         System.out.println("Now is: " + now);
+        
     }
 }
