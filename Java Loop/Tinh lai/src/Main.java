f0c558c8e72aa3c1d03db61e0383149e2ec46ecc
tungcnn <sontung.cnn@gmail.com>
2021-01-08 15:57:49 +0700

    Add Tinh lai

---
+++ Java Loop/Tinh lai/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double money = 1;
+        int month = 1;
+        double interest_rate = 1.0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter investment amount: ");
+        money = input.nextDouble();
+        System.out.println("Enter number of month: ");
+        month = input.nextInt();
+        System.out.println("Enter annual interest rate in percentage");
+        interest_rate = input.nextDouble();
+
+        double total_interest = 0;
+        for (int i = 0; i < month; i++) {
+            total_interest += money * (interest_rate/100)/12 * month;
+        }
+        System.out.println("Total of interest: " + total_interest);
+    }
+}
