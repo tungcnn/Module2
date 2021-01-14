6bb1537ed266b318868b488ad913d85df6eb6d23
tungcnn <sontung.cnn@gmail.com>
2021-01-14 14:17:02 +0700

    Add Abstract and Interface

---
+++ Abstract and Interface/Animal and Edible/src/AbstractAndInterfaceTest.java
@@ -0,0 +1,31 @@
+import animal.Animal;
+import animal.Chicken;
+import animal.Tiger;
+import edible.Edible;
+import fruit.Fruit;
+import fruit.Orange;
+import fruit.Apple;
+
+public class AbstractAndInterfaceTest {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[2];
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
+
+        for (Animal animal : animals) {
+            System.out.println(animal.makeSound());
+
+            if (animal instanceof Chicken) {
+                Chicken chicken = (Chicken) animal;
+                System.out.println(chicken.howToEat());
+            }
+        }
+        //
+        Fruit[] fruits = new Fruit[2];
+        fruits[0] = new Orange();
+        fruits[1] = new Apple();
+        for (Fruit fruit : fruits) {
+            System.out.println(fruit.howToEat());
+        }
+    }
+}
