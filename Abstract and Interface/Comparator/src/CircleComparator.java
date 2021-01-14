237f3c0d4de7d368008009373c37160ea90d5bd9
tungcnn <sontung.cnn@gmail.com>
2021-01-14 16:11:47 +0700

    Add comparator

---
+++ Abstract and Interface/Comparator/src/CircleComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle> {
+
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        return Double.compare(c1.getRadius(), c2.getRadius());
+    }
+}
