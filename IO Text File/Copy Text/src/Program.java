8f5f03ff2eb81b14e0a5e7a45bd29cb849ae71d0
tungcnn <sontung.cnn@gmail.com>
2021-01-27 15:03:35 +0700

    Add copy text

---
+++ IO Text File/Copy Text/src/Program.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Program {
+    public static void main(String[] args) throws IOException {
+        File ip = new File("in.txt");
+        File op = new File("out.txt");
+        copyFileUsingStream(ip, op);
+    }
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            assert is != null;
+            is.close();
+            assert os != null;
+            os.close();
+        }
+    }
+}
