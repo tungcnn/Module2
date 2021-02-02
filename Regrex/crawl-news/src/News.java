87297430eb8754d1ecf8248bb347b4046614b441
tungcnn <sontung.cnn@gmail.com>
2021-02-02 19:07:35 +0700

    Add crawl news

---
+++ Regrex/crawl-news/src/News.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class News {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+            // open the stream and put it into BufferedReader
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            // close scanner
+            scanner.close();
+            // remove all new line
+            content = content.replaceAll("\\n+", "");
+            // regex
+            Pattern p = Pattern.compile("Cate_TheGioi\\|MainList(.*?)title=(.*?)>(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println("Tin tức: " + m.group(3).substring(16));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
